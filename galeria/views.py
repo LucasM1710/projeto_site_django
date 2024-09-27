f3d7b21b3be2fc555c8a1613dcecda8ae974c7cf
Lucas Matheus <lucasmatheusc@hotmail.com>
2024-09-26 22:14:45 -0300

    Nomes dinamicos

--- galeria/views.py
+++ galeria/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 
 
+
 def index(request):
-    return render(request, 'galeria/index.html')
+    dados = {
+    1:{"nome":"Lucas corredor",
+       "legenda":"A corrida salva vidas!"},
+    2:{"nome":"Lucas atleta",
+       "legenda":"Sem pressa e sem pausa"}
+    }
+    return render(request, 'galeria/index.html',{"cards":dados})
 
 def imagem(request):
     return render(request, 'galeria/imagem.html')
