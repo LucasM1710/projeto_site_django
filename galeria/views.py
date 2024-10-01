eb5fabadce082d756fe67fd9d00e1a61c65c0e7e
Lucas Matheus <lucasmatheusc@hotmail.com>
2024-09-30 22:03:12 -0300

    Acessando o banco

--- galeria/views.py
+++ galeria/views.py
@@ -1,15 +1,11 @@
 from django.shortcuts import render
+from galeria.models import Fotografia
 
 
 
 def index(request):
-    dados = {
-    1:{"nome":"Lucas corredor",
-       "legenda":"A corrida salva vidas!"},
-    2:{"nome":"Lucas atleta",
-       "legenda":"Sem pressa e sem pausa"}
-    }
-    return render(request, 'galeria/index.html',{"cards":dados})
+    fotografias = Fotografia.objects.all()
+    return render(request, 'galeria/index.html',{"cards":fotografias})
 
 def imagem(request):
     return render(request, 'galeria/imagem.html')
